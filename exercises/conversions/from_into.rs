aaa72ba1fd48b9839581897952b19a520af8613c
Jeremy Fields <j.c.delafields@gmail.com>
2022-05-11 15:20:58 +0000

    done clippy, conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        // SOLVED split string into name and age
+        let (name, age) = match s.split_once(',') {
+            Some((name, age)) => (name.trim(), age.trim()),
+            _ => return Person::default(),
+        };
+
+        // cast age to a usize
+        if let Ok(age) = age.parse::<usize>() {
+            if name.len() > 0 {
+                return Person {
+                    name: String::from(name),
+                    age,
+                };
+            }
+        }
+
+        // SOLVED if the string is 0 return default
+        Person::default()
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,31 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        // SOLVED if string is empty
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        // SOLVED handle basic parsing
+        let split_string = s.split(",").collect::<Vec<&str>>();
+        let (name, age) = match &split_string[..] {
+            [name, age] => (
+                name.to_string(),
+                age.parse().map_err(ParsePersonError::ParseInt)?
+            ),
+            _ => return Err(ParsePersonError::BadLen)
+        };
+
+        // SOLVED handle no name
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        // SOLVED handle all good
+        Ok(Person {
+            name: name.into(),
+            age
+        })
     }
 }
 
